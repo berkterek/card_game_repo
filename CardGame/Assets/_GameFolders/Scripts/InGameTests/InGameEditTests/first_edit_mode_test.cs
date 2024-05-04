7bfa3175285c422c737fd84bb89050df82f9be62
berkterek <42406263+berkterek@users.noreply.github.com>
2024-05-04 12:52:02 +0300

    feat: core folders created

---
+++ CardGame/Assets/_GameFolders/Scripts/InGameTests/InGameEditTests/first_edit_mode_test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class first_edit_mode_test
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void first_edit_mode_testSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator first_edit_mode_testWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
