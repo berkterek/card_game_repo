f0da1417cddeb30bd48ceef5bfb7c4a912624ffe
berkterek <42406263+berkterek@users.noreply.github.com>
2024-05-04 18:20:08 +0300

    feat: cards can do rotate

--- CardGame/Assets/_GameFolders/Scripts/InGameScripts/Concretes/Controllers/CardController.cs
+++ CardGame/Assets/_GameFolders/Scripts/InGameScripts/Concretes/Controllers/CardController.cs
@@ -1,6 +1,8 @@
 using CardGame.Abstracts.Controllers;
 using CardGame.Abstracts.DataContainers;
 using CardGame.Helpers;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace CardGame.Controllers
@@ -9,13 +11,13 @@ namespace CardGame.Controllers
     {
         [SerializeField] SpriteRenderer _bodySpriteRenderer;
         [SerializeField] Transform _transform;
+        [SerializeField] bool _isFront = true;
 
         public ICardDataContainer CardDataContainer { get; private set; }
         public Transform Transform => _transform;
 
         void OnValidate()
         {
-            this.GetReference<SpriteRenderer>(ref _bodySpriteRenderer);
             this.GetReference<Transform>(ref _transform);
         }
 
@@ -24,5 +26,28 @@ namespace CardGame.Controllers
             CardDataContainer = cardDataContainer;
             _bodySpriteRenderer.sprite = cardDataContainer.CardSprite;
         }
+
+        [Button]
+        private void RotateCard()
+        {
+            _isFront = !_isFront;
+
+            if (_isFront)
+            {
+                _transform.DORotate(new Vector3(0f, 90f, 0f), 0.25f).onComplete += () =>
+                {
+                    _bodySpriteRenderer.sprite = CardDataContainer.CardSprite;
+                    _transform.DORotate(new Vector3(0f, 180f, 0f), 0.25f);
+                };
+            }
+            else
+            {
+                _transform.DORotate(new Vector3(0f, 90f, 0f), 0.25f).onComplete += () =>
+                {
+                    _bodySpriteRenderer.sprite = null;
+                    _transform.DORotate(new Vector3(0f, 0f, 0f), 0.25f);
+                };
+            }
+        } 
     }
 }
